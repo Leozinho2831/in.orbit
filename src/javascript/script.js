12e21513342fb1c77990bcd8f9e6dba1f073f801
Leozinho2831 <leoferraz1818@gmail.com>
2024-09-29 16:21:24 -0300

    commit: remove imports with comments added new goal test new verification to text title header new for of to verify if have goal completed

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,92 +1,64 @@
-// const dayjs = require("dayjs");
-// require('../../node_modules/dayjs/locale/pt-br')
-// import dayjs from '/node_modules/dayjs/dayjs.min.js';
-// import '/node_modules/dayjs/locale/pt-br.js'
-
-dayjs.locale('pt-br');
-console.log(dayjs());
-
 let goals = [];
 
-goals = [{
-    title: '',
-    desiredFrequency: 5,
-    completed: 2,
-}];
+goals = [
+    {
+        title: '',
+        desiredFrequency: 5,
+        completed: 0,
+    },
+    {
+        title: '',
+        desiredFrequency: 5,
+        completed: 2,
+    },
+];
 
 const sectionGoal = document.querySelector('.js-sectionGoal');
-const sectionInit = document.querySelector('.js-sectionInit')
+const sectionInit = document.querySelector('.js-sectionInit');
 const classSectionActive = 'js-activeSection';
 
-if(goals.length){
-    sectionGoal.classList.add(classSectionActive);
+if(sectionGoal && sectionInit){
+    if(!goals.length){
+        sectionInit.classList.add(classSectionActive);
+    } else {
+        sectionGoal.classList.add(classSectionActive);    
+    }    
+}
+
+// colocar as datas seguindo padrão português brasileiro
+dayjs.locale('pt-br');
+
+// código para determinar dias da página
+const firstDayOfWeek = dayjs().startOf('week');
+const lastDayOfWeek = dayjs().endOf('week');
+
+const titleHeader = document.querySelector('.js-titleHeader');
+
+// colchetes para usar string sem ser comandos do format do dayjs
+if(firstDayOfWeek.format('MM') != lastDayOfWeek.format('MM')){
+    titleHeader.textContent = 
+    `${firstDayOfWeek.format('DD [de] MMMM [à]')} ${lastDayOfWeek.format('DD [de] MMMM')}`;
 } else {
-    sectionInit.classList.add(classSectionActive);
+    titleHeader.textContent = 
+    `${firstDayOfWeek.format('DD [à]')} ${lastDayOfWeek.format('DD [de] MMMM')}`;
 }
 
-window.addEventListener('load', () => {
-    function initCreateGoal(){
-        const sectionCreateMeta = document.querySelector('.js-sectionGoalMenu');
-        const classCreateMeta = 'js-menuOpened';
+// goal é o objeto dentro do array
+// esse for é usado quando n é necessário o número do array para manipular ele
+for (const goal of goals){
+    const sectionCompletedGoal = document.querySelector('.js-completedGoal');
+    const classSectionCompletedGoal = 'js-haveCompletedGoal';
 
-        const sectionBlur = document.querySelector('.js-sectionBlur');
-    
-        function openRegisterGoal(){
-            sectionBlur.style.cssText = 'filter: blur(5px);';
-    
-            sectionCreateMeta.classList.toggle(classCreateMeta);
-        }
-    
-        // com type="module" deve-se determinar a função que quer usar no html como global, desta maneira.
-        window.openRegisterGoal = openRegisterGoal;
-    
-        function closeCreateGoal(event){
-            sectionBlur.style.cssText = 'filter: none;';
+    const textNotCompletedGoal = document.querySelector('.js-notCompletedGoal');
 
-            sectionCreateMeta.classList.remove(classCreateMeta);
-        }
-        
-        window.closeCreateGoal = closeCreateGoal;
+    if(goal.completed){
+        sectionCompletedGoal.classList.add(classSectionCompletedGoal);
+        textNotCompletedGoal.style.display = 'none';
+
+        // o break para o loop
+        break;
+    } else {
+        textNotCompletedGoal.style.display = 'block';
     }
-    
-    initCreateGoal();
-    
-    function initSelectRadio(){
-        const radios = document.querySelectorAll('input[type="radio"]');
-    
-        function toggleImageChecked(event){
-            const imagesRadios = document.querySelectorAll('.js-circle');
-    
-            imagesRadios.forEach((imageRadio) => {
-                    imageRadio.src = 'src/images/circle not checked.svg';
-                    imageRadio.alt = 'círculo não ativo';
-            });
-    
-            if(event.checked) {
-                const imageRadioChecked = document.querySelector(`.js-circle-${event.id}`);
-    
-                imageRadioChecked.src = 'src/images/circle checked.svg';
-                imageRadioChecked.alt = 'círculo ativo';
-            }
-        }
-    
-        radios.forEach((radio) => {
-    
-            radio.onchange = () => {
-                toggleImageChecked(radio);
-            }
-    
-        });
-    }
-    
-    initSelectRadio();
-});
 
-// não esquecer de usar essa função na hora de desativar o botão após completar todas as vezes a meta
-function disableButtonGoal(){
-    const buttonsDisabled = document.querySelector('.js-goalFinish');
-
-    buttonsDisabled.disabled = true;
 }
-
-disableButtonGoal();
