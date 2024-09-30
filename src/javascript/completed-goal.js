ef4fcc5e3d02888e9b0d4433e2652b0fa45245f3
Leozinho2831 <leoferraz1818@gmail.com>
2024-09-30 18:32:43 -0300

    commit: init config bar completed goals

--- src/javascript/completed-goal.js
+++ src/javascript/completed-goal.js
@@ -1,39 +1,63 @@
-const goals = JSON.parse(localStorage.getItem('goalsArray'));
+const getGoals = JSON.parse(localStorage.getItem('goalsArray'));
 
 // para debug
-if(goals){
-    if (dayjs(goals[0].createGoalDate, 'DD MM YYYY').isBefore(dayjs('30 09 2024', 'DD MM YYYY'))) {
+if(getGoals){
+    if (dayjs(getGoals[0].createGoalDate, 'DD MM YYYY').isBefore(dayjs('30 09 2024', 'DD MM YYYY'))) {
         localStorage.removeItem('goalsArray');
     }
 }
 
-function barCompletedGoals(){
-    const barCompletedGoal = document.querySelector('.js-completedGoalBar');
+const barCompletedGoal = document.querySelector('.js-completedGoalBar');
+
+const infoCompletedGoalNumber = document.querySelector('.js-completedGoalNumber');
+const infoTotalGoalNumber = document.querySelector('.js-totalGoalNumber');
+const infoPorcentCompleted = document.querySelector('.js-percentageCompletedGoal');
+
+export default function barCompletedGoals(){
+
+    const getGoals = JSON.parse(localStorage.getItem('goalsArray'));
 
     let goalCompletedTotal = 0;
     let totalGoals = 0;
 
-    if(goals){
-        goals.forEach((goal) => {
+    if(getGoals){
+        getGoals.forEach((goal) => {
             goalCompletedTotal += goal.completed;
             totalGoals += Number(goal.desiredFrequency);
         });
     }
 
-    const porcentCompleted = Math.round((100 * goalCompletedTotal) / totalGoals);
+    let porcentCompleted = Math.round((100 * goalCompletedTotal) / totalGoals);
+
+    const getValuesBar = JSON.parse(localStorage.getItem('barCompletedGoalValues'));
+
+    if(getValuesBar){
+        // infoCompletedGoalNumber.textContent = `${getValuesBar.infoCompletedGoal}`;
+        // infoTotalGoalNumber.textContent = `${getValuesBar.infoTotalGoal}`;
+        // infoPorcentCompleted.textContent = `${getValuesBar.infoPorcentCompletedBar}%`;
+
+        // barCompletedGoal.style.width = `${getValuesBar.infoPorcentCompletedBar}%`;
+
+        goalCompletedTotal = getValuesBar.infoCompletedGoal;
+        totalGoals = getValuesBar.infoTotalGoal;
+        porcentCompleted = getValuesBar.infoPorcentCompletedBar;
+    }
 
     barCompletedGoal.style.width = `${porcentCompleted}%`;
 
-    const infoCompletedGoalNumber = document.querySelector('.js-completedGoalNumber');
-    const infoTotalGoalNumber = document.querySelector('.js-totalGoalNumber');
-    const infoPorcentCompleted = document.querySelector('.js-percentageCompletedGoal');
-
-    infoCompletedGoalNumber.textContent = goalCompletedTotal;
-    infoTotalGoalNumber.textContent = totalGoals;
+    infoCompletedGoalNumber.textContent = `${goalCompletedTotal}`;
+    infoTotalGoalNumber.textContent = `${totalGoals}`;
     infoPorcentCompleted.textContent = `${porcentCompleted}%`;
-}
 
-barCompletedGoals();
+    const saveValuesBar = 
+    {
+        infoCompletedGoal: goalCompletedTotal,
+        infoTotalGoal: totalGoals,
+        infoPorcentCompletedBar: porcentCompleted,
+    }
+
+    localStorage.setItem('barCompletedGoalValues', JSON.stringify(saveValuesBar));
+}
 
 const containerButtonsGoal = document.querySelector('.js-containerButtonsGoal');
 const containerButtonsGoalStorage = localStorage.getItem('containerButtonsGoal');
@@ -42,31 +66,29 @@ if(containerButtonsGoalStorage){
     containerButtonsGoal.innerHTML = containerButtonsGoalStorage;
 }
 
-function createInfoGoalCompleted(){
-    goals.forEach((goal) => {
-        console.log('ta funfando', goal.completed)
-    });
-}
-
 function completedGoal(){
 
-    goals.forEach((goal) => {
+    getGoals.forEach((goal) => {
         const buttonCompleteGoal = document.querySelector(`.js-buttonCompleteGoal-${goal.id}`);
         const classButtonFinish = 'js-goalFinish';
+        goal.completed++;
 
         goal.completedHour = dayjs().format('HH[:]mm[h]');
 
         if(goal.completed == goal.desiredFrequency){
+            barCompletedGoals();
             buttonCompleteGoal.classList.add(classButtonFinish);
 
             const buttonsDisabled = document.querySelector('.js-goalFinish');
             buttonsDisabled.disabled = true;
         } else {
-            goal.completed++;
-            createInfoGoalCompleted();
             barCompletedGoals();
         }
     });
+
+}
+
+function createInfoGoalCompleted(){
 
 }
 
